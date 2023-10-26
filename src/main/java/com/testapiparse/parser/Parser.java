7eeae6daefa2dca343d315685f62dfcf40066603
user <shergazievdan@gmail.com>
2023-10-26 11:37:04 +0600

    added_finca_exch_parser_from_page

--- src/main/java/com/testapiparse/parser/Parser.java
+++ src/main/java/com/testapiparse/parser/Parser.java
@@ -1,4 +1,21 @@
 package com.testapiparse.parser;
 
+import lombok.*;
+import org.jsoup.*;
+import org.jsoup.nodes.*;
+import org.jsoup.select.*;
+
 public class Parser {
+    @SneakyThrows
+    public static void main(String[] args) {
+        try {
+            Document document = Jsoup.connect("https://www.fincabank.kg/").get();
+            var titleElements = document.select(".fif-planes-center");
+            for (var element: titleElements) {
+                System.out.println(element.text());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
